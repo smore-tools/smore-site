d2c109c8bc2bd35b49ed46db3f607a557e862f6c
natemoo-re <ntmoore@knox.edu>
2018-09-14 08:52:48 -0500

    add stencil

---
+++ stencil.config.ts
@@ -0,0 +1,10 @@
+import { Config } from '@stencil/core';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalStyle: 'src/global/app.css',
+  outputTargets: [
+    { type: 'www', dir: 'dist' }
+  ]
+};
