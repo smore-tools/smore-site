b6bdde30d7f17370327d4e3e5987431923b7d82b
natemoo-re <ntmoore@knox.edu>
2018-09-15 13:21:51 -0500

    Updates

---
+++ src/components/page-docs/icons.tsx
@@ -0,0 +1,7 @@
+export const Menu = () => (
+    <svg class="icon icon-menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 384h384v-42.666H64V384zm0-106.666h384v-42.667H64v42.667zM64 128v42.665h384V128H64z" /></svg>
+)
+
+export const Close = () => (
+    <svg class="icon icon-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z" /></svg>
+)
--- src/components/page-docs/page-docs.tsx
+++ src/components/page-docs/page-docs.tsx
@@ -1,20 +1,54 @@
 import { Component, Prop, State, Listen } from '@stencil/core';
+import { Menu, Close } from './icons';
 import { MatchResults } from '@stencil/router';
 
 
 @Component({
     tag: 'page-docs',
-    styleUrl: 'page-docs.css'
+    styleUrls: ['page-docs.css', 'menu.css']
 })
 export class Docs {
 
     @State() menuOpen = false;
     @Prop() match: MatchResults;
 
+    private toggleMenu(e: Event) {
+        this.menuOpen = !this.menuOpen;
+        e.stopPropagation();
+    }
+
     @Listen('click')
     protected clickHandler(e: MouseEvent) {
         if ((e.target as HTMLElement).id === 'menu-button') return;
         if (this.menuOpen) this.menuOpen = false;
+    }
+
+    private renderSubmenu(title: string, items: string[]) {
+        return (
+            <li class="submenu">
+                <h4>{ title }</h4>
+                <ul>
+                    {
+                        items.map(item => (
+                            <li>
+                                <stencil-route-link url={`/docs/${item.toLowerCase()}`}> {item} </stencil-route-link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </li>
+        )
+    }
+
+    private renderMenu() {
+        return (
+            <nav>
+                <ul class="menu">
+                    {this.renderSubmenu('Getting Started', ['Introduction', 'Installation'])}
+                    {this.renderSubmenu('Components', ['Async Content', 'Counter'])}
+                </ul>
+            </nav>
+        )
     }
 
     hostData() {
@@ -30,15 +64,12 @@ export class Docs {
             return (
                 <div class="page docs">
                     <aside>
-                        <nav>
-                            <ul>
-                                <li>Introduction</li>
-                                <li>Installation</li>
-                            </ul>
-                        </nav>
+                        {this.renderMenu()}
                     </aside>
                     <article>
-                        <button id="menu-button" onClick={() => this.menuOpen = true}>Menu</button>
+                        <button id="menu-button" class="icon-button" onClick={(e) => this.toggleMenu(e)}>
+                            { !this.menuOpen ? <Menu /> : <Close /> }
+                        </button>
                         <h1> {this.match.params.name}</h1>
                         <p>
                             Hello! My name is {this.match.params.name}.
