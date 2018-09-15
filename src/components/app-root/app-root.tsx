65e93c2f1d74f74b8d3f820cff53d76ff3ac8eea
natemoo-re <ntmoore@knox.edu>
2018-09-15 00:40:04 -0500

    add reroute from docs/

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -15,12 +15,13 @@ export class AppRoot {
             <stencil-route-switch scrollTopOffset={0}>
               <stencil-route url='/' component='page-home' exact={true} />
               <stencil-route url='/docs/:name' component='page-docs' />
+              <stencil-route url='/docs' routeRender={() => <stencil-router-redirect url='/docs/introduction'/>} />
               <stencil-route url='/demos' component='page-demos' />
               
               <stencil-route component='page-notfound'></stencil-route>
             </stencil-route-switch>
           </stencil-router>
-          {/* <stencil-router-redirect url='/docs/introduction' root='/docs' /> */}
+          {/*  */}
         </main>,
         <app-footer />
     ];
