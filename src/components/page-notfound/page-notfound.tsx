ce590574ac17e977536f35e07a0be6062de40f65
natemoo-re <ntmoore@knox.edu>
2018-09-15 14:38:55 -0500

    feat(): update 404 page

--- src/components/page-notfound/page-notfound.tsx
+++ src/components/page-notfound/page-notfound.tsx
@@ -10,13 +10,12 @@ export class PageNotfound {
     render() {
         return (
             <div class="page notfound">
-                <h1>Oops!</h1>
-                <h3>We couldn't find the page you were looking for...</h3>
-                <p>
-                    <stencil-route-link url='/'>
-                        Head home!
-                    </stencil-route-link>
-                </p>
+                
+                <img src="/assets/images/notfound.svg" alt="campfire"/>
+                <h2>We couldn't find the page you were looking for...</h2>
+                <stencil-route-link url='/'>
+                    <button>Head back to camp</button>
+                </stencil-route-link>
             </div>
         );
     }
