c5bbfc0b1eaa8144485637310c9bacdbda3e9dcf
natemoo-re <ntmoore@knox.edu>
2018-09-15 01:25:10 -0500

    begin docs menu

--- src/components/page-docs/page-docs.tsx
+++ src/components/page-docs/page-docs.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State, Listen } from '@stencil/core';
 import { MatchResults } from '@stencil/router';
 
 
@@ -8,16 +8,42 @@ import { MatchResults } from '@stencil/router';
 })
 export class Docs {
 
+    @State() menuOpen = false;
     @Prop() match: MatchResults;
+
+    @Listen('click')
+    protected clickHandler(e: MouseEvent) {
+        if ((e.target as HTMLElement).id === 'menu-button') return;
+        if (this.menuOpen) this.menuOpen = false;
+    }
+
+    hostData() {
+        return {
+            class: {
+                'menu-is-open': this.menuOpen
+            }
+        }
+    }
 
     render() {
         if (this.match && this.match.params.name) {
             return (
                 <div class="page docs">
-                    <p>
-                        Hello! My name is {this.match.params.name}.
-                        My name was passed in through a route param!
-                    </p>
+                    <aside>
+                        <nav>
+                            <ul>
+                                <li>Introduction</li>
+                                <li>Installation</li>
+                            </ul>
+                        </nav>
+                    </aside>
+                    <article>
+                        <button id="menu-button" onClick={() => this.menuOpen = true}>Menu</button>
+                        <h1> {this.match.params.name}</h1>
+                        <p>
+                            Hello! My name is {this.match.params.name}.
+                        </p>
+                    </article>
                 </div>
             );
         }
