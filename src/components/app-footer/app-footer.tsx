410706acf8deb708cb7e72674b6d39e56d5462ca
natemoo-re <ntmoore@knox.edu>
2018-09-14 20:44:49 -0500

    update copyright

--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -14,7 +14,7 @@ export class Footer {
             <footer>
                 <div class="container">
                     <p>Built with <Heart /> by <a href="https://twitter.com/n_moore">Nate Moore</a></p>
-                    <p class="license">Released under MIT License | Copyright @ 2018</p>
+                    <p class="license">Released under MIT License <span class="separator">/</span> © 2018</p>
                 </div>
             </footer>
         );
